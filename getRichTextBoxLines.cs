74c34d1bedbc0304bb9fce65ced8a798a9281135
Ego Trần <ego.bizzin@gmail.com>
2014-06-10 15:16:50 +0700

    Get text from RichTextBox with newline character

---
+++ getRichTextBoxLines.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ego
+{
+    public static partial class snippets
+    {
+        public static string getRichTextBoxLines(RichTextBox rtb) {
+            StringBuilder sb = new StringBuilder();
+            foreach (string line in rtb.Lines)
+                sb.Append(line + Environment.NewLine);
+            return sb.ToString();
+        }//getRichTextBoxLines
+    }//class
+}//namepsace
